665ed2c2cdf11c2b12b0ad752952389b488188c2
Suvanshsehgal <suvanshsehgal@gmail.com>
2025-12-28 16:23:12 +0530

    handle numpy types in FastAPI JSON responses

---
+++ services/json_utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def to_python(obj):
+    if isinstance(obj, dict):
+        return {k: to_python(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [to_python(v) for v in obj]
+    elif isinstance(obj, np.generic):
+        return obj.item()
+    else:
+        return obj
